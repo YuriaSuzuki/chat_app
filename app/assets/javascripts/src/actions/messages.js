5e59176036537d1e5e4b51f5596c23817fcc8c8c
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-04 17:56:25 +0900

    Fix actions for Api

--- app/assets/javascripts/src/actions/messages.js
+++ app/assets/javascripts/src/actions/messages.js
@@ -1,7 +1,49 @@
+import request from 'superagent'
 import Dispatcher from '../dispatcher'
-import {ActionTypes} from '../constants/app'
+import {ActionTypes, APIEndpoints, CSRFToken} from '../constants/app'
 
 export default {
+  // JSONの成形をどこでやるか？
+  getMessages() {
+    return new Promise(function(resolve, reject) {
+      request
+      .get('/api/messages')
+      .end(function(error, res) {
+        if (!error && res.status === 200) {
+          const json = JSON.parse(res.text)
+          Dispatcher.handleServerAction({
+            type: ActionTypes.GET_MESSAGES,
+            json,
+          })
+          resolve(json)
+        } else {
+          reject(res)
+        }
+      })
+    })
+  },
+
+  postMeessage(messageId) {
+    return new Promise(function(resolve, reject) {
+      request
+      .post(`${APIEndpoints.MESSAGE}`)
+      .set('X-CSRF-Token', CSRFToken())
+      .send({message_id: messageId}) // 送信したい内容
+      .end(function(error, res) {
+        if (!error && res.status === 200) {
+          const json = JSON.parse(res.text)
+          Dispatcher.handleServerAction({
+            type: ActionTypes.POST_MESSAGE,
+            messageId,
+            json,
+          })
+        } else {
+          reject(res)
+        }
+      })
+    })
+  },
+
   changeOpenChat(newUserID) {
     Dispatcher.handleViewAction({ // dispathcerに命令
       type: ActionTypes.UPDATE_OPEN_CHAT_ID,
