d52c9805d67bcca9739732cf754613843569b8ac
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-04 16:13:03 +0900

    Add api

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  class MessagesController < ApplicationController
+    def index
+      @messages = Message.all
+      render json: @messages
+    end
+
+    def create
+    end
+  end
+end
