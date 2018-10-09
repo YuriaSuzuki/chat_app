2c03908c3e84802172f275d7b56cfe2daeb010f4
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-09 11:33:50 +0900

    Enable to upload image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007023450) do
+ActiveRecord::Schema.define(version: 20181007102852) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "contents"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20181007023450) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
