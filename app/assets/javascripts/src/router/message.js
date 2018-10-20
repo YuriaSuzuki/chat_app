c084079c1f185cd17f4afefffb299731a7b08b71
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-20 17:00:44 +0900

    Enable to loadUsers

--- app/assets/javascripts/src/router/message.js
+++ app/assets/javascripts/src/router/message.js
@@ -1,16 +1,28 @@
 import ReactDecorator from '../base/react_decorator'
 import BaseRouter from '../base/router'
 import App from '../components/messages/app'
-import MessagesAction from '../actions/messages'
+// import MessagesAction from '../actions/messages'
+import UsersAction from '../actions/users'
+// import MessagesStore from '../stores/messages'
+import CurrentUserAction from '../actions/currentUser'
 
 export default class MessageRouter extends BaseRouter {
   register() {
-    this.route('/', this.decorateApp)
+    this.route('/', this.decorateApp, this.loadUsers, this.loadCurrentUser)
+  }
+
+  loadUsers(ctx, next) {
+    UsersAction.loadUsers()
+    next()
+  }
+
+  loadCurrentUser(ctx, next) {
+    CurrentUserAction.loadCurrentUser()
+    next()
   }
 
   decorateApp(ctx, next) {
     (new ReactDecorator()).decorate('react-main', App)
-    MessagesAction.getMessages()
     next()
   }
 }
