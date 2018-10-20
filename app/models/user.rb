9db1896ccb4103cb08b636f17d7590ce57a3b341
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-20 21:21:21 +0900

    Add methods for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,39 @@ class User < ActiveRecord::Base
     File.open("public/user_images/#{file_name}", "wb") {|f|f.write(file.read)}
     self.image = file_name
   end
+
+  def make_friend_with(user)
+    from_user_friendships.find_or_create_by(to_user_id: user.id)
+  end
+
+  def break_off_friend(user)
+      friendship = from_user_friendships.find_by(to_user_id: user.id) || to_user_friendships.find_by(from_user_id: user.id)
+      friendship.destroy if from_user_friendships
+  end
+
+  def find_friendship_for(user_id)
+    from_user_friendship = from_user_friendships.find_by(to_user_id: user_id)
+    to_user_friendship = to_user_friendships.find_by(from_user_id: user_id)
+    if from_user_friendship
+      from_user_friendship.to_user
+    else
+      to_user_friendship.from_user
+    end
+  end
+
+  def friends_all
+    friends_to_user + friends_from_user
+  end
+
+  def friend?(user)
+    self.from_friend?(user) || self.to_friend?(user)
+  end
+
+  def from_friend?(user)
+    friends_from_user.include?(user)
+  end
+
+  def to_friend?(user)
+    friends_to_user.include?(user)
+  end
 end
