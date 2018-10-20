f9f28fd5378888beb4977fd0fc894d645342d734
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-20 20:28:57 +0900

    Add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :messages
+  has_many :accesses
+  has_many :from_user_friendships, class_name: 'Friendship', foreign_key: 'from_user_id',
+           dependent: :destroy
+  has_many :friends_from_user, through: :from_user_friendships, source: 'to_user'
+  has_many :to_user_friendships, class_name: 'Friendship', foreign_key: 'to_user_id',
+           dependent: :destroy
+  has_many :friends_to_user, through: :to_user_friendships, source: 'from_user'
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  mount_uploader :image, ImagesUploader
+  validates :name, presence: true, uniqueness: true
 
-end
+  def set_image(file)
+    return if file.nil?
+    file_name = Time.zone.now.to_i.to_s + file.original_filename
+    File.open("public/user_images/#{file_name}", "wb") {|f|f.write(file.read)}
+    self.image = file_name
+  end
