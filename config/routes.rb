f24167e325f61716e6d4c6c693c4f017e1e87bc8
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-20 20:48:59 +0900

    Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,38 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  devise_for :users
-  get 'users/:id' => 'users#show'
-
-  namespace :api, { format: 'json' } do
-    resources :messages, :only => [:index, :create]
+  # 基本ルーティング
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    omniauth: 'users/omniauth',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks',
+  }
+  resources :users, only: [:show] do
+    collection do
+      get '/search', to: 'users#search'
+    end
   end
-
   root 'messages#index'
+  resources :messages, only: [:create]
+  resources :friendships, only: [:create, :destroy]
+
+  # APIルーティング
+  namespace :api, defaults: { format: :json } do
+    resources :messages, only: [:index, :create] do
+      collection do
+        post :upload_image
+      end
+    end
+    resources :users, only: [:index, :show, :create] do
+      collection do
+        get :search
+      end
+    end
+    resources :current_user, only: [:index] do
+      collection do
+        put :update
+      end
+    end
+  end
 end
