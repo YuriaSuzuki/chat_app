df830432a4fe043d60ec65a1fc3d1915f9141675
YuriaSuzuki <yuriairi9207@gmail.com>
2018-10-20 20:24:25 +0900

    Add friendships controller

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,24 @@
+class FriendshipsController < ApplicationController
+
+  def create
+    @user = User.find(params[:to_user_id])
+    if current_user.friend?(@user)
+      redirect_to root_path
+    else
+      current_user.make_friend_with(@user)
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user.friend?(@user)
+      friendship = current_user.find_friendship_for(params[:id])
+      current_user.break_off_friend(friendship)
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+end
